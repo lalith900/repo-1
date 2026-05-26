9f0e75263d3a90d0ce29067c4d60c62acb4aa68f
lalith900 <lalithakrishna332@gmail.com>
2026-05-26 16:00:24 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Backend Running');
+  res.send('Backend is Running');
 });
 
 app.listen(5000, () => {
